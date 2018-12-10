c0f28408587d6ea4ac81603875db90e41ad6aaa7
Yevgeniy Kryukov <evgkrukov@gmail.com>
2018-12-10 15:40:45 +0600

    Add file index.php (replace cinema.php) and add comments in file DataBase.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>PHP Cinema</title>
+</head>
+
+<frameset rows="100,1*" cols="*" frameborder="NO" framespacing="3">
+	<frame src="Search.php" name="Top" scrolling="NO" frameborder="NO">
+	<frameset cols="285,1*" frameborder="NO" framespacing="3">
+		<frame name="Order" src="Order.php" frameborder="NO">
+		<frame name="Main" src="TopPicks.php" frameborder="NO">
+	</frameset>
+</frameset>
+<noframes><body bgcolor="#FFFFFF">
+
+</body></noframes>
+</html>
--- model/DataBase.php
+++ model/DataBase.php
@@ -3,12 +3,12 @@ require("config.php");
 
 class DataBase 
 {
-
+    // Поключаемся к БД
     public static function db_connect() {
         return pg_connect("host=".DBHOST." port=".DBPORT." dbname=".DBNAME." user=".DBUSER." password=".DBPASS);
     }
 
-
+    // Выполняем запрос к БД
     public static function db_query($link1,$squery,$apar = null ) {
         if ($link1 == null) $link = self::db_connect(); else $link = $link1;
         if ($apar == null) $result = pg_query( $link, $squery ); else $result = pg_query_params( $link, $squery, $apar );
